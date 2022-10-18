ad1df2b45b8cc0993c6f0f8eb05594863a40d447
Saif Abuelhasan <saiofa99@gmail.com>
2022-10-18 13:13:18 +0200

    fix build errors

--- src/routes/toppings.ts
+++ src/routes/toppings.ts
@@ -1,22 +1,23 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../common/middleware";
 import { create, read, readAll, update, remove } from "../common/crud";
-import prisma from "../db";
+import { ToppingRepo } from "../db/toppings";
 
 const router = express.Router();
+const repo = new ToppingRepo();
 
-router.get("/", readAll(prisma.topping.findMany));
+router.get("/", readAll(repo));
 
 router.use(isAuthenticated);
 
 router.use(isAdmin);
 
-router.get("/:id", read(prisma.topping.findUnique));
+router.get("/:id", read(repo));
 
-router.post("/", create(prisma.topping.create));
+router.post("/", create(repo));
 
-router.put("/:id", update(prisma.topping.update));
+router.put("/:id", update(repo));
 
-router.delete("/:id", remove(prisma.topping.delete));
+router.delete("/:id", remove(repo));
 
 export default router;
