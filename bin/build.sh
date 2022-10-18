bfdff4885dab4c1012d9a09f3c18f809490be8fd
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-18 23:52:53 +0200

    included prisma schema

--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,1 @@
-npm install . && npm run clean && tsc && npx prisma generate && cp -rf build/ www/ && cp -R .elasticbeanstalk www/.elasticbeanstalk && cp .npmrc www/.npmrc && cp package.json www/package.json && cd www && zip -r Archive.zip . && cd ..
+npm install . && npm run clean && tsc && cp -rf build/ www/ && cp -R .elasticbeanstalk www/.elasticbeanstalk && cp .npmrc www/.npmrc && cp package.json www/package.json && cp -rf prisma/ www/ && cd www && zip -r Archive.zip . && cd ..
