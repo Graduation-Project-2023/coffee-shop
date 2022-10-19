71b502987bea006b9ffa361b06f454d12087d0f8
Saif Abuelhasan <saiofa99@gmail.com>
2022-10-19 20:47:41 +0200

    .

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm run build && eb list && eb use coffee-shop && eb deploy && chmod +x bin/eb_setenv.sh && bin/eb_setenv.sh
+eb list && eb use coffee-shop && eb deploy && chmod +x bin/eb_setenv.sh && bin/eb_setenv.sh
