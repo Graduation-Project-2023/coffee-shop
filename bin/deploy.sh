a44975d59451fa3190e5c6f1909ab7b218d34dba
Saif Abuelhasan <saiofa99@gmail.com>
2022-10-19 18:29:59 +0200

    remove build from circleci

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-eb use coffee-shop && eb deploy && chmod +x bin/eb_setenv.sh && bin/eb_setenv.sh
+npm run build && eb list && eb use coffee-shop && eb deploy && chmod +x bin/eb_setenv.sh && bin/eb_setenv.sh
