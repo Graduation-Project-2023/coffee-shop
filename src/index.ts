28178d200d7fdeee74cfd346bbc958772f6fc97c
Saif Abuelhasan <saiofa99@gmail.com>
2022-10-19 14:14:44 +0200

    allow cors

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes";
 import { resourceNotFound } from "./common/middleware";
+import cors from "cors";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", routes);
 
