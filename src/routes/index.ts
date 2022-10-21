48e58f09b8f55654b5f23f08c50cbb806a5a6f24
Saif Abuelhasan <saiofa99@gmail.com>
2022-10-21 17:48:16 +0200

    added toppings route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,12 @@ import express from "express";
 import auth from "./auth";
 import product from "./products";
 import order from "./orders";
-
+import topping from "./toppings";
 const router = express.Router();
 
 router.use("/auth", auth);
 router.use("/products", product);
+router.use("/toppings", topping);
 router.use("/orders", order);
 
 export default router;
